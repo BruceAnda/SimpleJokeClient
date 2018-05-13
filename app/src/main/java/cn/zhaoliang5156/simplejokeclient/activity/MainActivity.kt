0d140d35b097bb68789561f238d2de6cac1f7610
zhaoliang <2668645098@qq.com>
2018-05-13 21:52:00 +0800

    添加主界面

---
+++ app/src/main/java/cn/zhaoliang5156/simplejokeclient/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.zhaoliang5156.simplejokeclient.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cn.zhaoliang5156.simplejokeclient.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
