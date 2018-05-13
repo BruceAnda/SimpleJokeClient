8d27a0682b45901a3083444cb0355c0dae550345
zhaoliang <2668645098@qq.com>
2018-05-13 22:18:38 +0800

    封装数据模型类

--- app/src/main/java/cn/zhaoliang5156/simplejokeclient/activity/net/JokeModels.kt
+++ app/src/main/java/cn/zhaoliang5156/simplejokeclient/activity/net/JokeModels.kt
@@ -5,3 +5,41 @@ package cn.zhaoliang5156.simplejokeclient.activity.net
  * joke 数据模型
  * version 1.0
  */
+
+// 文本笑话响应模型类
+class Response(
+        var showapi_res_body: ResponseBody,
+        var showapi_res_code: Int,
+        var showapi_res_error: String
+) {
+    override fun toString(): String {
+        return "Response(showapi_res_body='$showapi_res_body', showapi_res_code=$showapi_res_code, showapi_res_error='$showapi_res_error')"
+    }
+}
+
+// 文本笑话响应体模型类
+class ResponseBody(
+        var totalNum: Int,
+        var ret_code: Int,
+        var list: ArrayList<TextJoke>
+)
+
+// 文本笑话模型类
+class TextJoke(
+        var title: String,
+        var content: String
+) {
+    override fun toString(): String {
+        return "TextJoke(title='$title', content='$content')"
+    }
+}
+
+// 图片笑话模型类
+class ImageJoke(
+        var title: String,
+        var sourceurl: String
+) {
+    override fun toString(): String {
+        return "ImageJoke(title='$title', sourceurl='$sourceurl')"
+    }
+}
